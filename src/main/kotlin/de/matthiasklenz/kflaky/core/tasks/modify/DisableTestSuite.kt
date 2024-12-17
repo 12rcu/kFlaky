f73fd08060e5f64a42e799b14d9c5b39f3162c88
matthias <matthias.klenz@outlook.com>
2024-12-17 22:14:12 +0100

    fix disable suite import

--- src/main/kotlin/de/matthiasklenz/kflaky/core/tasks/modify/DisableTestSuite.kt
+++ src/main/kotlin/de/matthiasklenz/kflaky/core/tasks/modify/DisableTestSuite.kt
@@ -1,7 +1,9 @@
 package de.matthiasklenz.kflaky.core.tasks.modify
 
+import de.matthiasklenz.kflaky.core.plattform.Plattform
 import de.matthiasklenz.kflaky.core.project.TestFramworkConfig
 
 fun disableTestSuit(config: TestFramworkConfig, content: String): String {
-    return TextModifyUtil.addFirstAnnotationBefore(content, config.testSuiteStart(), config.ignoreAnnotation())
+    val contentWithImports = TextModifyUtil.addImports(content, config.imports().joinToString(Plattform.lineSeperator), config.importStart())
+    return TextModifyUtil.addFirstAnnotationBefore(contentWithImports, config.testSuiteStart(), config.ignoreAnnotation())
 }
